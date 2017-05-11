431bfff550c16085556be492475cc52781c50747
Miroslav Simko <mirsimko@gmail.com>
2017-05-11 06:42:31 -0700

    script was cleaned a little

--- cleanPicoList.sh
+++ cleanPicoList.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 
 fileList=picoList_all.list
+lastDay=107
 # output file
 cleanedList=$( echo "$fileList" | sed 's/.list//g' )_cleaned.list
 
 cp "$fileList" "$cleanedList"
-for dayNumber in $( seq -w 107 ); do
+for dayNumber in $( seq -w $lastDay ); do
   grep -v '/P16id/'"$dayNumber"'/' "$cleanedList" > temp.list # remove lines with this day number
   mv temp.list "$cleanedList"
 done
