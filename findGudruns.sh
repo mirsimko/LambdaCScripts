cba0dffebcbb1bc7cd18c1eac0c85f773198b4c9
Miroslav Simko <mirsimko@gmail.com>
2017-11-13 15:39:17 -0500

    couple of comments

--- findGudruns.sh
+++ findGudruns.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# script for finding good runFiles from good run file list
+
+
 filename="$1"
+# list of good runs
 listsDir="$2"
+# fileList
 goodRuns="${3:-goodRuns.txt}"
+# fileList with sorted runs
 
 if [ ! -f "$filename" ]; then
   echo Filelist not found
