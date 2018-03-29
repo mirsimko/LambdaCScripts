8fde3aedae126aa124345ffeb2f11c8cbb17b57b
Miroslav Simko <mirsimko@gmail.com>
2018-03-29 17:44:25 -0400

    changed files written as an array

--- stPicoEventify.sh
+++ stPicoEventify.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-StPicoEventFiles=StPicoEvent.h StPicoTrack.h StPicoBTofPidTraits.h
+StPicoEventFiles=(StPicoEvent.h StPicoTrack.h StPicoBTofPidTraits.h)
+echo changing \#include path for ${StPicoEventFiles[*]}
 
-for codeFile in StRoot/*/*.h StRoot/*/*.cxx; do
-  for picoEventFile in $StPicoEventFiles; do
+for codeFile in StRoot/*/*.h StRoot/*/*.cxx StRoot/*/*.C; do
+  for picoEventFile in ${StPicoEventFiles[*]}; do
     sed -i 's|StPicoDstMaker/'"$picoEventFile"'|StPicoEvent/'"$picoEventFile"'|g' "$codeFile"
   done
 done
