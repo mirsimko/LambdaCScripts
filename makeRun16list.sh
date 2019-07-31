f33f3bc49e05e5d95dff50a7a11762c3d4fc417d
Miroslav Simko <mirsimko@gmail.com>
2019-07-31 05:38:04 -0400

    rights updated

--- makeRun16list.sh
+++ makeRun16list.sh
@@ -6,6 +6,7 @@ stSstStreamList=st_sstStream_pico"$now".list
 
 echo Creating pico.list ...
 get_file_list.pl -keys path,filename -cond production=P16ij,trgsetupname=AuAu_200_production_2016,filetype=daq_reco_picoDst,filename~st_physics,storage=local -limit 0 -delim / -distinct > "$stPhysicsStreamList"
+
 echo Creating st_sstStream_pico.list ...
 get_file_list.pl -keys path,filename -cond production=P16ij,trgsetupname=AuAu200_production2_2016,filetype=daq_reco_picoDst,filename~st_sst,storage=local,daynumber[]169-179 -limit 0 > "$stSstStreamList"
 
